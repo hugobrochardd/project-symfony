1a4b7c8131218cb312efe6f72fbb2b9419c97d1a
hugobrochardd <144666846+hugobrochardd@users.noreply.github.com>
2024-03-18 10:25:48 +0100

    test codespace

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Movie;
+use App\Entity\Category;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ApiController extends AbstractController
+{
+    #[Route('/api', name: 'app_api')]
+    public function index(): Response
+    {
+        return $this->render('api/index.html.twig', [
+            'controller_name' => 'ApiController',
+        ]);
+    }
+
+    #[Route('/api/movie/{id}', name: 'app_api_movie')]
+    public function readMovie(Movie $mov, SerializerInterface $serializer ): Response
+    {
+      $data = $serializer->normalize($mov, null, ['groups' => 'json_movie']);
+      $response = new JsonResponse( $data );
+      return $response;
+    }
+    
+    #[Route('/api/category/{id}', name: 'app_api_category')]
+    public function readcategory(category $mov, SerializerInterface $serializer ): Response
+    {
+      $data = $serializer->normalize($mov, null, ['groups' => 'json_category']);
+      $response = new JsonResponse( $data );
+      return $response;
+    }
+
+}
